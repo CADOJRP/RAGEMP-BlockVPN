8df4e45c864a41bd9ec518fff384a6f2fce3a689
Avery Johnson <avery627@gmail.com>
2018-05-21 20:35:01 -0400

    Removed Used Config Value

--- blockVPN/index.js
+++ blockVPN/index.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 
 // Configuration
-var ownerEmail = 'avery627@gmail.com';             // Owner Email (Required) - No account needed (Used Incase of Issues)
+var ownerEmail = '';             // Owner Email (Required) - No account needed (Used Incase of Issues)
 var kickThreshold = 0.99;        // Anything equal to or higher than this value will be kicked. (0.99 Recommended as Lowest)
 var kickReason = 'We\'ve detected that you\'re using a VPN or Proxy. If you belive this is a mistake please contact the administration team.';
 var printFailed = true;
